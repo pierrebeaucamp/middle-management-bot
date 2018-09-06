b96922428c7b1daba097320ee30ab5c5595a32c3
Pierre Beaucamp <mail@pierrebeaucamp.com>
2018-09-06 06:44:13 -0400

    Initial commit

---
+++ default.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ? import <nixpkgs> { inherit system; },
+  system ? builtins.currentSystem,
+  nodejs ? pkgs."nodejs-8_x"
+}:
+
+let
+  nodeEnv = import ./node-env.nix {
+    inherit (pkgs) stdenv python2 utillinux runCommand writeTextFile;
+    inherit nodejs;
+  };
+  nodePackages = import ./node-packages.nix {
+    inherit (pkgs) fetchurl fetchgit;
+    inherit nodeEnv;
+  };
+in
+  nodePackages // {
+    package = nodePackages.package.override (attrs: {
+      postInstall = "npm run build";
+    });
+  }
