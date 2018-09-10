37018b0e379edca384fe674a18d9df7ba953f9c3
Pierre Beaucamp <mail@pierrebeaucamp.com>
2018-09-10 09:19:40 -0400

    Fixes typo

--- deployment.tf
+++ deployment.tf
@@ -40,6 +40,6 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb = 128
   source_archive_bucket = "${google_storage_bucket.bucket.name}"
   source_archive_object = "${google_storage_bucket_object.archive.name}"
-  trigget_http = true
+  trigger_http = true
   entry_point = "main"
 }
